ddb8851f90fbc9b7a35bb7d83fc5eddf1000f588
LinaNgubombi <ngubombilina@gmail.com>
2025-12-02 12:18:17 +0200

    just created a vuejs app

---
+++ chefmaster/src/components/AllCourses.vue
@@ -0,0 +1,47 @@
+<script>
+    export default{
+        data(){
+            return{
+                courses: [
+  {
+    id: 1,
+    title: "Mastering Italian Pasta",
+    price: 299,
+    skill_level: "Beginner",
+    available: false,
+    img: "italian-pasta.jpg"
+  },
+  {
+    id: 2,
+    title: "Sushi Rolling Essentials",
+    price: 350,
+    skill_level: "Intermediate",
+    available: false,
+    img: "sushi-rolling.jpg"
+  },
+  {
+    id: 3,
+    title: "French Pastry Techniques",
+    price: 420,
+    skill_level: "Advanced",
+    available: false,
+    img: "french-pastry.jpg"
+  },
+  {
+    id: 4,
+    title: "Vegan Cooking Fundamentals",
+    price: 250,
+    skill_level: "Beginner",
+    available: false,
+    img: "vegan-cooking.jpg"
+  }
+]
+
+            }
+        }
+    }
+</script>
+<template>
+    <h1>Courses Must apppear here</h1>
+</template>
+<style></style>
---
+++ chefmaster/src/views/CoursesView.vue
@@ -0,0 +1,6 @@
+<script setup>
+import AllCourses from '@/components/AllCourses.vue';
+</script>
+<template>
+    <AllCourses/>
+</template>
